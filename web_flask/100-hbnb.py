20887ff1003d3e443fb718686b92ccb39afc354d
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-24 22:52:38 +0000

    web_flask corr4

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,7 @@ from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.place import Place
+from models.user import User
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -20,8 +21,9 @@ def hbnb():
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
     places = storage.all(Place).values()
+    users = storage.all(User)
     return render_template('100-hbnb.html', states=states,
-                           amenities=amenities, places=places)
+                           amenities=amenities, places=places, users=users)
 
 
 if __name__ == '__main__':
